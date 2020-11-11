a39448fc0e01694db845f656869a68cdf203062f
enzoesper <enzoesper@hotmail.com>
2020-11-10 21:57:07 -0300

    adding first query methods

--- src/main/java/com/esper/demomarket/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/esper/demomarket/persistence/crud/ProductoCrudRepository.java
@@ -3,6 +3,11 @@ package com.esper.demomarket.persistence.crud;
 import com.esper.demomarket.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+import java.util.List;
+import java.util.Optional;
 
+public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+    List<Producto> findByIdCategoriaOOrderByNombreAsc(int idCategoria);
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 }
